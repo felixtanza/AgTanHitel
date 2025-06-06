8ab831e467386966744f4d091600b442f6064156
felixtanza <felixtanzaotienoofficial@gmail.com>
2025-06-06 20:44:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const orderRoutes = require('./routes/orderRoutes');
+const orderRoutes = require('./backend/routes/orderRoutes');
 
 dotenv.config();
 const app = express();
